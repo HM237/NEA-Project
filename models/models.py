ae1885dceac5f011be6ae0c3df1c93eb2abf5d95
HM237 <hudaifa032007@outlook.com>
2024-12-18 13:23:39 +0000

    renaming, reordering and spacing models

--- models/models.py
+++ models/models.py
@@ -8,11 +8,12 @@ class User:
         self.phone_number = phone_number
         self.date_of_birth= date_of_birth
     
-    def add(self):
+    def add_User(self):
         with sqlite3.connect('test2.db') as conn:
             cursor = conn.cursor()
             cursor.execute('INSERT INTO  User (FirstName, LastName, Email, PhoneNumber, DoB) VALUES (?,?,?,?,?)', (self.first_name,self.last_name,self.email,self.phone_number, self.date_of_birth))
             conn.commit()
+
 #Nikah class which inserts into Nikah Table
 class Nikah:
     def __init__(self, user_id,groom_first_name, groom_last_name, bride_first_name, bride_last_name, time, date, post_code, address_line):
@@ -26,12 +27,28 @@ class Nikah:
         self.post_code = post_code
         self.address_line = address_line
 
-    def add(self):
+    def add_Nikah(self):
         with sqlite3.connect('test2.db') as conn:
              cursor = conn.cursor()
              cursor.execute('INSERT INTO  Nikah (UserID,GroomFirstName,GroomLastName,BrideFirstName,BrideLastName,Time, Date, PostCode,AddressLine) VALUES (?,?,?,?,?,?,?,?,?)', (self.user_id, self.groom_first_name,self.groom_last_name, self.bride_first_name, self.bride_last_name, self.time, self.date, self.post_code, self.address_line))
 
              conn.commit()  
+
+#Madrasah class which inserts into Madrasah Table
+class Madrasah:
+    def __init__(self, user_id, time, date, child_fname, child_lname, child_date_of_birth ):
+        self.user_id = user_id
+        self.time = time
+        self.date = date
+        self.child_fname = child_fname
+        self.child_lname = child_lname
+        self.child_date_of_birth = child_date_of_birth
+
+    def add_Madrasah(self):
+        with sqlite3.connect('test2.db') as conn:
+             cursor = conn.cursor()
+             cursor.execute('INSERT INTO  Madrasah (UserID,Time, Date, ChildFirstName,ChildLastName ,ChildDoB) VALUES (?,?,?,?,?,?)', (self.user_id,self.time,self.date,self.child_fname,self.child_lname,self.child_date_of_birth ))
+
 #Payment class which inserts into Payment Table
 class Payment:
     def __init__(self, user_id,post_code, address_line,payment_method, CVC, price):
@@ -43,25 +60,13 @@ class Payment:
         self.price = price
 
 
-    def add(self):
+    def add_Payment(self):
         with sqlite3.connect('test2.db') as conn:
              cursor = conn.cursor()
              cursor.execute('INSERT INTO  Payment (UserID,PaymentMethod,AddressLine,PostCode,CVC, Price) VALUES (?,?,?,?,?,?)', (self.user_id, self.payment_method, self.address_line, self.post_code, self.CVC, self.price))
              conn.commit()
-#Madrasah class which inserts into Madrasah Table
-class Madrasah:
-    def __init__(self, user_id, time, date, child_fname, child_lname, child_date_of_birth ):
-        self.user_id = user_id
-        self.time = time
-        self.date = date
-        self.child_fname = child_fname
-        self.child_lname = child_lname
-        self.child_date_of_birth = child_date_of_birth
 
-    def add(self):
-        with sqlite3.connect('test2.db') as conn:
-             cursor = conn.cursor()
-             cursor.execute('INSERT INTO  Madrasah (UserID,Time, Date, ChildFirstName,ChildLastName ,ChildDoB) VALUES (?,?,?,?,?,?)', (self.user_id,self.time,self.date,self.child_fname,self.child_lname,self.child_date_of_birth ))
+
 #Clashed class which checks for unavailable bookings
 class Clashed:
     def __init__(self,time, date):
